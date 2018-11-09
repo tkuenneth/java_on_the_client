7b412210b4acf525b27848212db1b555a215501c
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2018-11-09 15:35:52 +0100

    added demo of jnlp services

--- WebStartApp/src/com/thomaskuenneth/webstartapp/FXMLDocumentController.java
+++ WebStartApp/src/com/thomaskuenneth/webstartapp/FXMLDocumentController.java
@@ -1,34 +1,54 @@
 package com.thomaskuenneth.webstartapp;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javax.jnlp.BasicService;
+import javax.jnlp.ServiceManager;
+import javax.jnlp.UnavailableServiceException;
 
 /**
  *
  * @author tkuenneth
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Label label;
 
+    @FXML
+    private Hyperlink link;
+
     private int count;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         count += 1;
         update();
     }
-    
+
+    @FXML
+    private void handleHyperlinkAction(ActionEvent event) {
+        try {
+            BasicService bs = (BasicService) ServiceManager.lookup("javax.jnlp.BasicService");
+            bs.showDocument(new URL(link.getText()));
+        } catch (MalformedURLException | UnavailableServiceException ex) {
+            // should be logging this exception
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         count = 0;
         update();
-    }    
+    }
 
     private void update() {
         String s;
